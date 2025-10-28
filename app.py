d315e5f85e75a86ae1fe73f2786295f3a7e3a13f
dhruvyellanki19 <dhruvsai@umd.edu>
2025-10-27 20:49:14 -0400

    updated app.py

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 from langchain_community.llms import Ollama
-import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-
 load_dotenv()
 
-
-langchain_api_key = os.getenv("LANGCHAIN_API_KEY")
-project_name = os.getenv("LANGCHAIN_PROJECT_NAME", "local_project")
-
-
-if langchain_api_key:
-    os.environ["LANGCHAIN_API_KEY"] = langchain_api_key
+if "LANGCHAIN_API_KEY" in st.secrets:
+    os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
     os.environ["LANGCHAIN_TRACING_V2"] = "true"
-    os.environ["LANGCHAIN_PROJECT_NAME"] = project_name
+    os.environ["LANGCHAIN_PROJECT_NAME"] = st.secrets.get("LANGCHAIN_PROJECT_NAME", "simple_genai_demo")
 else:
-    st.warning("LANGCHAIN_API_KEY not found. Tracing will be disabled.")
+
+    langchain_api_key = os.getenv("LANGCHAIN_API_KEY")
+    project_name = os.getenv("LANGCHAIN_PROJECT_NAME", "local_project")
+
+    if langchain_api_key:
+        os.environ["LANGCHAIN_API_KEY"] = langchain_api_key
+        os.environ["LANGCHAIN_TRACING_V2"] = "true"
+        os.environ["LANGCHAIN_PROJECT_NAME"] = project_name
+    else:
+        st.warning("LANGCHAIN_API_KEY not found. Tracing will be disabled.")
 
 
 st.title("🦙 Simple Retrieval GenAI Demo with LLaMA2 Model")
-
 
 input_text = st.text_input("💭 What question do you have in mind?")
 
@@ -39,6 +41,9 @@ chain = prompt | llm | output_parser
 
 if input_text:
     with st.spinner("Thinking..."):
-        response = chain.invoke({"question": input_text})
-    st.success("Response:")
-    st.write(response)
+        try:
+            response = chain.invoke({"question": input_text})
+            st.success("Response:")
+            st.write(response)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
